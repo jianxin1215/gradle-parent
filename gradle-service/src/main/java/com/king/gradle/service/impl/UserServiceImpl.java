aa1646461ca6cd1fd70223ddbd6f85a5561b1a67
King <noemail@qq.com>
2018-02-24 16:37:10 +0800

    更新用户服务

---
+++ gradle-service/src/main/java/com/king/gradle/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.king.gradle.service.impl;
+
+import com.king.gradle.api.User;
+import com.king.gradle.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User get(long id) {
+        return null;
+    }
+
+    @Override
+    public long save(User user) {
+        return 0;
+    }
+
+    @Override
+    public long delete(long id) {
+        return 0;
+    }
+
+    @Override
+    public List<User> list() {
+        return null;
+    }
+}
